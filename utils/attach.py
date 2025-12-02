e66fe45f1739186a2329185db758b6f3e6782fad
Dmitriy <BuldyshkinDmitriy@yandex.ru>
2025-12-02 16:11:55 +0300

    fixed attach.py

--- utils/attach.py
+++ utils/attach.py
@@ -3,42 +3,30 @@ from allure_commons.types import AttachmentType
 
 
 def add_screenshot(browser):
-    screenshot = browser.driver.get_screenshot_as_png()
-    allure.attach(
-        screenshot,
-        name="Screenshot",
-        attachment_type=AttachmentType.PNG
-    )
+    png = browser.driver.get_screenshot_as_png()
+    allure.attach(png, name='Screenshot', attachment_type=AttachmentType.PNG)
 
 
-def add_page_source(browser):
-    page_source = browser.driver.page_source
-    allure.attach(
-        page_source,
-        name="Page Source",
-        attachment_type=AttachmentType.HTML
-    )
+def add_logs(browser):
+    log = "".join(f"{entry['level']} - {entry['message']}\n"
+                  for entry in browser.driver.get_log('browser'))
+    allure.attach(log, name="Browser logs", attachment_type=AttachmentType.TEXT)
 
 
-def add_browser_logs(browser):
-    logs = browser.driver.get_log("browser")
-    log_text = "\n".join([f"{log['level']}: {log['message']}" for log in logs])
-    allure.attach(
-        log_text,
-        name="Browser Logs",
-        attachment_type=AttachmentType.TEXT
-    )
+def add_html(browser):
+    html = browser.driver.page_source
+    allure.attach(html, name='Page source', attachment_type=AttachmentType.HTML)
 
 
-def add_video(session_id):
+def add_video(browser):
     video_url = (
-        f"https://selenoid.autotests.cloud/video/{session_id}.mp4"
+        f"https://selenoid.autotests.cloud/video/{browser.driver.session_id}.mp4"
     )
-
     allure.attach(
-        f'<html><body><video width="100%" height="100%" controls>'
-        f'<source src="{video_url}" type="video/mp4"></video></body></html>',
-        name="Video",
+        f'<html><body><video width="100%" controls>'
+        f'<source src="{video_url}" type="video/mp4">'
+        f'</video></body></html>',
+        name='Video',
         attachment_type=AttachmentType.HTML,
-        extension=".html"
+        extension='.html'
     )
